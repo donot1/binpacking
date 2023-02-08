26d01989f80da425a847a689bc3b84258b7dfb53
donot1 <kalghatgin@gmail.com>
2023-02-08 07:04:02 -0600

    renamed file

--- firstfit.cpp
+++ firstfit.cpp
@@ -1,6 +1,7 @@
 #include <iostream> 
 #include <fstream> 
 #include <string> 
+#include <vector>
 using namespace std; 
 
 vector<int> readFromFile (string file) {
@@ -14,23 +15,23 @@ vector<int> readFromFile (string file) {
   return items; 
 }
 
-int firstFit (vector<int> &items, int bin_size) {
-  int number_bins = 0; 
-  multiset<int> capacities; 
-  for (int item : item
+
+int firstFit(int binCapacity, vector<int> items) {
+    vector<int> bins; 
+    bins.push_back(0); 
+    for (int i = 0; i < items.size(); i++) {
+        int item = items[i]; 
+        bool placed = false; 
+        for (int j = 0; j < bins.size(); j++) {
+        if (bins[j] + item <= binCapacity) {
+            bins[j] += item; 
+            placed = true; 
+            break; 
+        }
+        }
+        if (!placed) {
+        bins.push_back(item); 
+        }
+    }
+    return bins.size(); 
 }
-
-int main(int argc, char* argv[]) {
-  if (argc < 1) {
-    cout << "USAGE: ./firstfit input_file bin_size\n"; 
-    cout << "OUTPUT: number_bins\n";
-  }
-
-  string inputFile = argv[1]; 
-  int bin_size = stoi(argv[2]); 
-  vector<int> item_sizes = readFromFile(inputFile); 
-  
-  return 0; 
-}
-
-
