c005f2924ebb8eefbc9a34e0baaded2e58e0a159
donot1 <kalghatgin@gmail.com>
2023-02-07 18:33:49 -0600

    push

--- firstfit.cpp
+++ firstfit.cpp
@@ -1,10 +1,34 @@
 #include <iostream> 
+#include <fstream> 
+#include <string> 
 using namespace std; 
+
+vector<int> readFromFile (string file) {
+  ifstream inputFile; 
+  inputFile.open(file); 
+  vector<int> items; 
+  int item; 
+  while (inputFile >> item) {
+    items.push_back(item); 
+  }
+  return items; 
+}
+
+int firstFit (vector<int> &items, int bin_size) {
+  int number_bins = 0; 
+  multiset<int> capacities; 
+  for (int item : item
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 1) {
-    cout << "USAGE: ./firstfit input_file\n"; 
+    cout << "USAGE: ./firstfit input_file bin_size\n"; 
     cout << "OUTPUT: number_bins\n";
   }
+
+  string inputFile = argv[1]; 
+  int bin_size = stoi(argv[2]); 
+  vector<int> item_sizes = readFromFile(inputFile); 
+  
   return 0; 
 }
